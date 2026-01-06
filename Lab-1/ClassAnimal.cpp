d449b63131a119510c0a2016c06965e84653c579
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-01-06 21:37:59 +0600

    Create Animal class with methods for info display
    Define Animal class with attributes and methods for printing info and name.

---
+++ Lab-1/ClassAnimal.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Animal{
+public:
+  string color;
+  string sound;
+  string name;
+  void printinfo(){
+  cout<<color<<" "<<sound<<" "<<endl;
+  }
+  void showname(){
+  cout<<name<<endl;
+  }
+};
+int main(){
+  animal cat;
+  cat.color;
+  cat.sound;
+  cat.name;
+  cat.printinfo();
+  cat.showname();
+
+return 0;
+}
