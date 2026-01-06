df1eadc5452cede6934639c18a6530cd66c8c5fd
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-01-06 20:08:18 +0600

    Add Fibonacci recursive function implementation

---
+++ Lab-1/FibonacciRec.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int fibo(int n){
+  if(n==1) return 0;
+  else if(n==2) return 1;
+  else return fibo(n-1)+fibo(n-2);
+}
+
+int main(){
+int num;
+cout<<"Enter the number : ";
+cin>>num;
+
+cout<<num<<"th fibonacci number = "<<fibo(num)<<endl;
+
+cout<<"The fibonacci series = ";
+for(int i=1;i<=num;i++){
+  cout<<fibo(i)<<" ";
+}
+
+return 0;
+}
