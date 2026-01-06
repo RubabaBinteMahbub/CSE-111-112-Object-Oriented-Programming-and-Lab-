8ef2c6c9a192266b49c826036ca417b9a89cf279
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-01-06 23:57:12 +0600

    Add Leap Year determination program

---
+++ Lab-1/LeapYear.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int year; 
+cout<<"Enter the year : ";
+cin>>year;
+if(year%400==0 || year%4==0 && year%100!=0) cout<<"Leap year"<<endl;
+else cout<<"Not leap yar"<<endl;
+return 0;
+}
