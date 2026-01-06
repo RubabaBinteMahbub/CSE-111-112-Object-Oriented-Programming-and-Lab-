db4771983baa45565b06bd8e6c2595baaef1c968
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-01-06 19:58:34 +0600

    Add recursive factorial function in factRec.cpp

---
+++ Lab-1/factRec.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+     if(n==0 || n==1) return 1;
+     return n*factorial(n-1);
+}
+
+int main(){
+int num;
+cout<<"Enter the number : ";
+cin>>num;
+
+cout<<"Factorial of "<<num<<" = "<<factorial(num)<<endl;
+return 0;
+}
