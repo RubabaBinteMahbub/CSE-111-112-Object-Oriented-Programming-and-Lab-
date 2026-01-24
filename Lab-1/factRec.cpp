64b7fc57286cb5ed9bde1c5dbbbee06bc5a30b2e
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-01-24 21:20:59 +0600

    Improve readability of factorial function
    Refactor factorial function for better readability.

--- Lab-1/factRec.cpp
+++ Lab-1/factRec.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 
 int factorial(int n){
-     if(n==0 || n==1) return 1;
-     return n*factorial(n-1);
+     if(n==0 || n==1){
+       return 1;
+     }
+     else{
+       return n*factorial(n-1);
+     }
 }
 
 int main(){
