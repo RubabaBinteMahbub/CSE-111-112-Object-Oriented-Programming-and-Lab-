f277feec43ae47d44c6ecac5685a67f664b988f4
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-02-10 21:41:58 +0600

    Refactor Department class and print function
    Refactored Department class and print function syntax.

--- Lab - 5/1.cpp
+++ Lab - 5/1.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-class Department;
+class Varsity;
+
+class Department{
+  public:
+     void print(Varsity &ob);
+};
 
 class Varsity{
-  private :
-    string name;
-  protected :
-    string location;
-  public :
+   private :
+     string name;
+   protected :
+     string location;
+   public :
      Varsity(){
         name = "DU";
         location = "Dhaka";
@@ -17,18 +23,16 @@ class Varsity{
      friend void Department :: print (Varsity &ob);
 };
 
-class Department{
-  public :
-     void print(Varsity &ob){
+     void Department :: print(Varsity &ob){
      cout<<"Name of varsity : "<<ob.name<<endl;
      cout<<"Location of the varsity : "<<ob.location<<endl;
      }
-};
+
 int main(){
-      Varsity v1;
-      Department d1;
-      d1.print(v1);
-      return 0;
+    Varsity v1;
+    Department d1;
+    d1.print(v1);
+    return 0;
 }
 // কোনো member বা non-member অর্থাৎ global function কে কোনো class এর friend বানালে সেই ফাংশনটা থেকে ওই class এর private ও protected attribute কে শুধু ডট অপারেটর দিয়েই access করা যাবে (অন্য public attribute গুলোর মতো)।
 // অন্য কোনো ক্লাসের member function কে friend বানাতে হলে Scope resolution দিয়ে সেই ক্লাসের নামটাও লিখতে হবে।
