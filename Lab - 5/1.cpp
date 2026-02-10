4b78aba0720303e0a698ff5047a806de6394a8d5
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-02-10 21:19:05 +0600

    Add Varsity and Department classes with friend function
    Implement Varsity and Department classes with friend function to access private and protected members.

---
+++ Lab - 5/1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Department;
+
+class Varsity{
+  private :
+    string name;
+  protected :
+    string location;
+  public :
+     Varsity(){
+        name = "DU";
+        location = "Dhaka";
+     }
+    
+     friend void Department :: print (Varsity &ob);
+};
+
+class Department{
+  public :
+     void print(Varsity &ob){
+     cout<<"Name of varsity : "<<ob.name<<endl;
+     cout<<"Location of the varsity : "<<ob.location<<endl;
+     }
+};
+int main(){
+      Varsity v1;
+      Department d1;
+      d1.print(v1);
+      return 0;
+}
+// কোনো member বা non-member অর্থাৎ global function কে কোনো class এর friend বানালে সেই ফাংশনটা থেকে ওই class এর private ও protected attribute কে শুধু ডট অপারেটর দিয়েই access করা যাবে (অন্য public attribute গুলোর মতো)।
+// অন্য কোনো ক্লাসের member function কে friend বানাতে হলে Scope resolution দিয়ে সেই ক্লাসের নামটাও লিখতে হবে।
+// তবে যে class এর member function কে friend বানাবো তাকে ওপরে আগে declare করে আসতে হবে (forward declaration)। নাহলে যার friend বানাবো সে friend এর class কে চিনবে না।
