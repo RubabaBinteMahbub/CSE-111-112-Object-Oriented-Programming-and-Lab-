4626bad86db7df94ff3f6c13d72c1b398ef8c0d6
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-02-12 02:43:17 +0600

    Add Student struct and main function for marks input

---
+++ Lab - 4/6.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct Student{
+  string name;
+  int roll;
+  int marks[3];
+
+  Student(string n, int r, int m[]){
+    name=n;
+    roll=r;
+    for(int i=0; i<3; i++){
+    marks[i]=m[i]; // Array direct assign করা যায় না (marks = m )। ভেক্টর করা যায়।
+    }
+  }
+
+   void display(){
+        int sum=0;
+     
+        for(int i=0; i<3; i++){
+				sum+=marks[i];
+        }
+     
+        cout<<"Average of marks : "<<sum/3.0<<endl;
+    }
+      
+};
+
+int main(){
+    string n1;
+    int r1, m1[3];
+  
+    cout<<"Enter name and roll : ";
+    cin>>n1>>r1;
+    cout<<"Enter marks : ";
+    for(int i=0; i<3; i++){
+      cin>>m1[i];
+    }
+
+    Student S1(n1, r1, m1);
+    S1.display();
+
+    return 0;
+}
