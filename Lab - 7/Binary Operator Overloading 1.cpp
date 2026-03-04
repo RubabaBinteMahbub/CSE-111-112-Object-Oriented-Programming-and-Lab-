94944130f6d71573342017a290f0c2cebfe9104b
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-03-04 21:32:59 +0600

    Implement binary operator overloading for Complex class

---
+++ Lab - 7/Binary Operator Overloading 1.cpp
@@ -0,0 +1,37 @@
+// +, -,*, / egulo binary operator
+// Karon kaj korte 2ta vatriable ba objecgt lage
+
+// Binary operator overloading
+
+#include<bits/stdc++.h>
+using namespace std;
+class Complex{
+private:
+    int real, imag;
+public:
+    Complex(int r=0, int i=0){
+    real=r;
+    imag=i;
+    }
+
+    Complex operator+(Complex const &obj){
+    Complex res;
+    res.real=real+obj.real;
+    res.imag=imag+obj.imag;
+    return res;
+    }
+
+    void print(){
+    cout<<real<<" + "<<imag<<"i"<<endl;
+    }
+};
+int main(){
+     Complex c1(1,2), c2(3,4);
+     c1.print();
+     c2.print();
+
+     Complex c3=c1+c2;
+     c3.print();
+
+     return 0;
+}
