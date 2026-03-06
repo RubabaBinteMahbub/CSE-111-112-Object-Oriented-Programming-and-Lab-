514622c9f0dce599e87b7d55e11c698ff196b68b
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-03-06 23:20:28 +0600

    Enhance comments and formatting in operator overloading
    Added comments and improved formatting for clarity.

--- Lab - 7/Binary Operator Overloading 1.cpp
+++ Lab - 7/Binary Operator Overloading 1.cpp
@@ -1,6 +1,8 @@
 // +, -,*, / egulo binary operator
 // Karon kaj korte 2ta vatriable ba objecgt lage
 
+// 2 ta object +/- korte bolle (kore 3rd 1 ta object a rakhte bolle) bujhbo operator overloading er kotha bujhieche
+
 // Binary operator overloading
 
 #include<bits/stdc++.h>
@@ -15,7 +17,7 @@ public:
     }
 
     Complex operator+(Complex const &obj){
-    Complex res;
+    Complex res; 
     res.real=real+obj.real;
     res.imag=imag+obj.imag;
     return res;
@@ -35,3 +37,12 @@ int main(){
 
      return 0;
 }
+
+// Default onstructor is mandetory for operator overloading
+/*
+Else you can also use
+Complex operator+(Complex &ob){
+    Complex res(real+ob.real, imag+ob.imag);
+    return res;
+}
+*/
