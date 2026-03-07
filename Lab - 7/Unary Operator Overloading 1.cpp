1cedddc73b3bba5dc64c2c0d4109369fde2f69d9
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-03-07 13:19:56 +0600

    Add unary operator overloading for Distance class
    Implemented unary operator overloading to decrement feet and inches.

---
+++ Lab - 7/Unary Operator Overloading 1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Distance{
+public:
+int feet, inch;
+
+Distance(int f=0, int i=0){
+feet=f;
+inch=i;
+}
+
+void operator-(){
+feet--;
+inch--;
+cout<<"\nFeet and inches decrement : "<<feet<<" "<<inch<<endl;
+}
+};
+
+int main(){
+    Distance d1(8, 9);
+    -d1;
+    return 0;
+}
