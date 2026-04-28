83f7fbb90494c7245bf4472168c2e0402bbb1e46
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-04-28 21:57:05 +0600

    Add unary '-' operator overloading for Distance
    Implement unary operator overloading for Distance class

---
+++ CW/Unary Oprator Overloading 2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class Distance{
+public:
+    int feet, inch;
+
+    Distance(int f=0, int i=0){
+        feet = f;
+        inch = i;
+    }
+
+    // Unary - operator returning object
+    Distance operator-(){
+        Distance res;
+        res.feet = feet--;
+        res.inch = inch--;
+        return res;
+    }
+
+    void display(){
+        cout << "Feet and inches : " << feet << " " << inch << endl;
+    }
+};
+
+int main()
+{
+    Distance d1(8,9);
+    Distance d2;
+    d2 = -d1;
+
+    d1.display();
+    d2.display();
+
+    return 0;
+}
